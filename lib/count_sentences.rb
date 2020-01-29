7d73aee30b0825d920d956b55efe888c21aef045
Dan Foley <dan@dan-foley.com>
2020-01-29 18:51:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,8 +17,9 @@ class String
   def count_sentences
     if !self.empty? && self.size > 1
       split_sentences = self.split(".")
+      binding.pry
       split_sentences.each do |n|
-        binding.pry
+        
       end
     else
       0
